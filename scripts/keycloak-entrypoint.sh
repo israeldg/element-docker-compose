5eae7b3c7e934249c0bedb70df4e0096572e8be1
Israel De La Rosa Guel <44977652+israeldg@users.noreply.github.com>
2025-03-05 20:01:11 -0600

    asd

--- scripts/keycloak-entrypoint.sh
+++ scripts/keycloak-entrypoint.sh
@@ -2,7 +2,10 @@
 
 # launch keycloak with secrets from disk
 
-
+# Read the secrets from the files and export them as environment variables
 export KEYCLOAK_ADMIN_PASSWORD=$(</run/secrets/keycloak_password)
 export KC_DB_PASSWORD=$(</run/secrets/postgres_password)
 
+
+# Start Keycloak
+exec /opt/keycloak/bin/kc.sh start
